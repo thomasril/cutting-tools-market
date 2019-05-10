2fe58a91d88daa39f7fe143cec38b4fe336e7305
thomasril <thomas.asril98@gmail.com>
2019-05-10 16:51:32 +0700

    first commit

---
+++ resources/views/404.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Astspv
+ * Date: 5/9/2019
+ * Time: 1:57 PM
+ */
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,28 @@
+@extends('layout/master')
+
+@section('sub_intro')
+    <div class="container">
+        <div class="row">
+            <div class="span4">
+                <h3>Profile</h3>
+            </div>
+            <div class="span8">
+                <ul class="breadcrumb notop">
+                    <li><a href="#">Home</a><span class="divider">/</span></li>
+                    <li class="active">Profile</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('main_content')
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+                Profile
+            </div>
+        </div>
+
+    </div>
+@stop
